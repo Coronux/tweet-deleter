fd68e6eb53f11958fea517b46c53c1a0e81a826c
Luna <contactariadev@gmail.com>
2022-12-08 12:31:27 +0100

    Create destroy.py

---
+++ destroy.py
@@ -0,0 +1,15 @@
+import tweepy
+
+consumer_key = "your_consumer_key"
+consumer_secret = "your_consumer_secret"
+access_token = "your_access_token"
+access_token_secret = "your_access_token_secret"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+tweets = api.user_timeline()
+
+for tweet in tweets:
+    api.destroy_status(tweet.id)
